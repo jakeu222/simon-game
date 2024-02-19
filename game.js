03868df1e6b3d9b2f6c4b1f6ef68bcce2bf3d354
jakeu222 <81273955+jakeu222@users.noreply.github.com>
2024-02-19 12:52:51 -0700

    completed Simon Game

---
+++ game.js
@@ -0,0 +1,98 @@
+var buttonColors =["red","blue","green","yellow"]
+
+
+var gamePattern = []
+
+var userClickedPattern = []
+
+var level = 0
+
+var started = false
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("level " + level)
+        nextSquence()
+        started = true
+    }
+})
+
+
+$(".btn").click(function(){
+    var userChosenColor = $(this).attr("id")
+
+    userClickedPattern.push(userChosenColor)
+
+    playSound(userChosenColor)
+    animatePress(userChosenColor)
+    checkAnswer(userClickedPattern.length-1)
+})
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success")
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSquence()
+                },1000)
+
+            
+        }
+
+    }else {
+        console.log("wrong")
+
+        playSound("wrong")
+
+        $("body").addClass("game-over")
+        setTimeout(function(){
+            $("body").removeClass("game-over")
+        }, 200)
+
+        $("#level-title").text("Game Over, Press Any Key to Restart")
+        startOver()
+    }
+}
+
+function nextSquence(){
+
+    userClickedPattern =[]
+
+    level++
+
+    $("#level-title").text("level " + level)
+
+
+    var randomNumber = Math.floor(Math.random()*4)
+    
+    var randomChosenColor = buttonColors[randomNumber]
+    
+    gamePattern.push(randomChosenColor)
+    
+    $("#" +randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
+    
+    playSound(randomChosenColor)
+    
+    
+}
+
+function playSound(name){
+
+    var audio = new Audio("sounds/" + name + ".mp3")
+    audio.play()
+
+}
+
+function animatePress(currentColor){
+        $("#" + currentColor).addClass("pressed")
+
+        setTimeout(function(){
+        $("#" + currentColor).removeClass("pressed")
+        },100)
+}
+
+function startOver(){
+level = 0
+gamePattern = []
+started = false
+}
